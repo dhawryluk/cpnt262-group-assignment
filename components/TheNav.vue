049faa62f7c9350f099af7a89a01175fd728d46c
DKH <derek.hawryluk@edu.sait.ca>
2024-03-12 22:25:16 -0600

    dark mode toggle - incomplete

--- components/TheNav.vue
+++ components/TheNav.vue
@@ -1,6 +1,6 @@
 <template>
   <nav
-    class="flex flex-wrap backdrop-blue-sm bg-white/30 items-center justify-between p-4 bg-background w-full"
+    class="flex flex-wrap backdrop-blur-sm bg-white/30 items-center justify-between p-4 w-full"
   >
     <NuxtLink to="/" class="text-white text-3xl"
       ><i class="text-3xl text-white hover:drop-shadow-lg"></i
@@ -20,6 +20,15 @@
         <NuxtLink to="/products">Products</NuxtLink>
       </li>
     </ul>
+    <div class="flex items-center">
+      <h2 class="text-white px-4">Dark Mode</h2>
+      <label class="inline-block h-[34px] relative w-[60px]" for="checkbox">
+        <input class="appearance-none" type="checkbox" id="checkbox" />
+        <div
+          class="before:translate-x-[26px] before:checked:translate-x-[26px] before:checked:bg-[00e9f9] before:rounded-[34px] before:bg-[#fffffa] before:bottom-1 before:h-[26px] before:w-[26px] before:left-1 before:absolute before:ease-in-out rounded-full bg-[#172026] bottom-0 cursor-pointer left-0 absolute right-0 top-0 transition-[0.4s]"
+        ></div>
+      </label>
+    </div>
   </nav>
 </template>
 
