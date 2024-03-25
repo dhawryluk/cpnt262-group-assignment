2d66cad69ed0b667820d5591a86ea600ae637d99
ShawnArdeshiri <Shayan.Ardeshiri@edu.sait.ca>
2024-03-25 12:26:27 -0700

    news and footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,35 @@
 <template>
-  <div><p>&copy</p></div>
+  <footer class="pt-40 pr-0">
+    <div>
+      <div>
+      <ul class="flex flex-wrap justify-center gap-x-8">
+        <li><a href="#"><img src="/assets/images/logo-facebook.svg" class="w-12 h-12 text-white"/></a></li>
+        <li><a href="#"><img src="/assets/images/logo-youtube.svg" class="w-12 h-12 stroke-white"/></a></li>
+        <li><a href="#"><img src="/assets/images/logo-instagram.svg" class="w-12 h-12"/></a></li>
+        <li><a href="#"><img src="/assets/images/logo-twitter.svg" class="w-12 h-12"/></a></li>
+      </ul>
+      
+      </div>
+      <div>
+        <p class="flex justify-end pr-80">Email Us:</p>
+      </div>
+      
+      <div>
+        <ul class=" flex flex-wrap justify-end pr-48 pt-8 gap-x-24">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Games</a></li>
+          <li><a href="#">News</a></li>
+          <li><a href="#">About</a></li>
+          <input type="search" class="peer justify-start relative z-10 h-8 w-80 rounded-full border bg-transparent outline-none" />
+        </ul>
+        
+      </div>
+
+      <div class="justify-end">Email
+
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script setup></script>
