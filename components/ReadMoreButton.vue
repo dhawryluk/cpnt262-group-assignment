a1a38f27197b863512e74c6f3e0550ef2dca5aed
DKH <derek.hawryluk@edu.sait.ca>
2024-03-26 18:50:45 -0600

    fixing read me link

--- components/ReadMoreButton.vue
+++ components/ReadMoreButton.vue
@@ -1,9 +1,10 @@
 <template>
   <button
-    class="readMoreButton text-md w-24 h-10 text-darkColor bg-lightColor dark:ring-accent1 ring-2 ring-accent2 ring-inset cursor-pointer rounded-tl-lg rounded-br-lg py-2 hover:scale-105 hover:ring-accent2/90"
+    class="readMoreButton lazy text-md w-24 h-10 text-darkColor bg-lightColor dark:ring-accent1 ring-2 ring-accent2 ring-inset cursor-pointer rounded-tl-lg rounded-br-lg py-2 hover:scale-105 hover:ring-accent2/90"
   >
-    <a href="../pages/news.vue"></a>
-    <slot>Read More</slot>
+    <NuxtLink href="/news">
+      <slot>Read More</slot>
+    </NuxtLink>
   </button>
 </template>
 
